9b63e9c538676d624cd0d0e21d8ca14c48ee457e
unknown <2127651@labinfo.is.escuelaing.edu.co>
2019-12-02 17:12:42 -0500

    Solucionados errores maricas

--- Proyecto/ultima sustent/Bermudez - Laiton/Proyecto 2/SQL/POBLAR OK/Poblada easy.sql
+++ Proyecto/ultima sustent/Bermudez - Laiton/Proyecto 2/SQL/POBLAR OK/Poblada easy.sql
@@ -45,7 +45,20 @@ VALUES
 INSERT INTO Accidentes(NoAccidente, Detalle, DetalleEspe, ValorArreglo, IdAbogado, Fecha, DetalleXML)
 VALUES
 (
-0, 'CHOQUELATERAL', 'DAÃODEPUERTASYVENTANAS', 1000, 100000, TO_DATE('20/12/1998','DD/MM/YYYY'),'');
+0, 'CHOQUELATERAL', 'DAÃODEPUERTASYVENTANAS', 1000, 100000, TO_DATE('20/12/1998','DD/MM/YYYY'),'<?xml version="1.0" encoding="utf-8"?>
+    <DetalleXML>
+		<Informacion numeroaccidente= "0" >
+			<Partesafectada>Puerta trasera derecha y puerta delantera izquierda </Partesafectada>
+			<Lugar>Centro de Bogota</Lugar>
+			<Hora>2</Hora>
+			<LeyesInfrigidas>no respertar un pare y exceso de velocidad </LeyesInfrigidas>
+			<NumeroHeridos>2</NumeroHeridos>
+			<NumeroMuertos>0</NumeroMuertos>
+			<ChoquecontraVehiculo>Si</ChoquecontraVehiculo>
+            <ChoquecontraPeaton>No</ChoquecontraPeaton>
+            <ChoquecontraEstructuras>No</ChoquecontraEstructuras>
+		</Informacion>
+	</DetalleXML>');
 
 
 INSERT INTO LoteProducciones(CodigoInicio, CodigoFin, InicioLote, FinLote, Descripcion)
