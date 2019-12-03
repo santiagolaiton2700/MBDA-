53621a47409915da9c64db98b796cc834f386e3c
santiagolaiton2700 <santiago.laiton@mail.escuelaing.edu.co>
2019-12-03 09:48:01 -0500

    eliminado basura

--- Proyecto/Proyecto 2/Proyecto/SQL/CONSULTAS/consultas.sql
+++
@@ -1,9 +0,0 @@
--------------Consultar informacion del vehiculo con placas rco609-------
-select *
-from vehiculo
-where placa='rco609';
-
--------------Consultar la informacion del abogado que asitio a el accidente-------------
-select identificacion,Tprofesional,nombre,apellido,telefono,correo 
-from (abogado join accidente on(abogado.identificacion=accidente.iabogado))
-where accidente.n_accidente=2213;
